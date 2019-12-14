9a9c4cac767760bcc5398dc19bec2c343f4a98ba
Anna Semrau <01153099@pw.edu.pl>
2019-12-14 20:51:01 +0100

    v0.1 algorytmu podstawienia wstecznego + test

--- trunk/src/lmp9/backsubst.c
+++ trunk/src/lmp9/backsubst.c
@@ -1,22 +1,36 @@
 #include "backsubst.h"
+#include <stdio.h>
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+	int i, j;
+	double suma;
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+	if(mat->r != mat->c){
+		fprintf(stderr, "nieprawidlowy rozmiar macierzy!\n");
+		return 2;
+	}
+	for (i = (x->r)-1; i > 0; i--) { //-1, bo indeksy!
+		suma = 0;
+		for(j=(x->r)-1; j>i; j--){ //od ostat. wiersza, do wiersza w ktorym jestesmy
+			suma = suma - x->data[j][0]*mat->data[i][j]; //
+		}
+		if(mat->data[i][i] != 0){ //zrobilismy przestawianie, czyli macierz musi byc nieosobliwa
+								  //no i jestesmy na 'stopniu' macierzy schodkowej
+			x->data[i][0] = (b->data[i][0] + suma) / mat->data[i][i];//x dla danego wiersza staje sie suma 
+			                                                         //wyrazu wolnego i suma dotychcz. podzielona
+																	 // przez wspolczynnik przy x
+		}else{
+			fprintf(stderr, "macierz jest osobliwa, dzielenie przez zero!!!\n");
+			return 1;
+		}
+		
+	}
+	return 0;
 }
 
 
