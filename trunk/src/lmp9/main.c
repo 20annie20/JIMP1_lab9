040ad373d66d1e466e06486c2ce019f10539624c
Anna Semrau <01153099@pw.edu.pl>
2019-12-19 23:09:40 +0100

    uzupełnienie obsługi błędów, scope testu wynikow

--- trunk/src/lmp9/main.c
+++ trunk/src/lmp9/main.c
@@ -4,41 +4,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void errorHandle(int *p){
-	if(p == NULL)
-		printf("Test - nie odczytano danych\n");
-}
-
-void errorHandle(int *p){
-	if(p == NULL)
-		fprintf(stderr, "Test - nie odczytano danych\n");
-}
 
 int main(int argc, char ** argv) {
 	int res;
 	Matrix * A = readFromFile(argv[1]);
 	Matrix * b = readFromFile(argv[2]);
 	Matrix * x;
+	//uzywane do testu
+	if(argc > 2){
+		Matrix *compare = readFromFile(argv[3]);
+		if(compare == NULL) 
+			fprintf(stderr, "nie moge wczytac danych do porownania wynikow!\n");
+	}
+
 
 	if (A == NULL){
-		errorHandle(NULL);
+		fprintf(stderr, "nie moge wczytac danych poprawnie\n");
 		return -1;
 	}
 	if (b == NULL){
-		errorHandle(NULL);
+		fprintf(stderr, "nie moge wczytac danych poprawnie\n");
 		return -2;
 	}
+
+
 	printToScreen(A);
 	printToScreen(b);
 
 	if(A->r != b->r){
-		errorHandle(NULL);
+		fprintf(stderr, "nieprawidlowe rozmiary macierzy!\n");
+		return -3;
 	}
 	
 	//reorganizacja macierzy
 	A = findElem(A, A->r, A->c, 0);
 	printToScreen(A);
 	res = eliminate(A,b);
+	if(res != 0 ){
+		fprintf(stderr,"brak elementu niezerowego do umieszczenia na diagonali. nie mozemy dzielic przez 0!\n");
+	}
 	x = createMatrix(b->r, 1);
 	if (x != NULL) {
 
@@ -49,6 +53,10 @@ int main(int argc, char ** argv) {
 	  	freeMatrix(x);
 	} else {
 		fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+	}
+
+	for(int i = 0; i < x->r; i++){
+		;//sprawdzenie czy dane otrzymane zgadzaja sie w jakims przyblizeniu z oczekiwanymi
 	}
 
 	freeMatrix(A);
