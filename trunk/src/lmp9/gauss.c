82bb37dde4481df0cef681f8de93267c647f92df
Anastasiya <n.rebkovets2002@gmail.com>
2019-12-20 00:08:04 +0100

    fixed

--- trunk/src/lmp9/gauss.c
+++ trunk/src/lmp9/gauss.c
@@ -7,20 +7,19 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+	
 for (int i=0; i < mat->r - 1; i++)
 		{
+			if (mat->c != mat->r)
+			return 2;
+
 			double m;
 			
+			findElem (mat, mat->c, mat->r, i);
+			if (mat->data[i][i] == 0)
+					return 1;
 			for (int j=i+1; j< mat->r; j++) 
 				{					
-					if (mat->data[i][i] == 0)
-					return 1;
-					
-					findElem (mat, mat->c, mat->r, i);
-					
 					m = mat->data[j][i]/mat->data[i][i];
 					b->data[j][0] -= m*(b->data[i][0]);
 					
--- trunk/src/lmp9/main.c
+++ trunk/src/lmp9/main.c
@@ -17,12 +17,34 @@ int main(int argc, char ** argv) {
 	printToScreen(b);
 
 	res = eliminate(A,b);
+
+	if (res == 1)
+	{
+    fprintf (stderr, "Błąd elimincji! Dzielenie przez 0! \n");
+		freeMatrix(A);
+		freeMatrix(b);
+		return EXIT_FAILURE;
+	}
+
 	x = createMatrix(b->r, 1);
 	if (x != NULL) {
 		res = backsubst(x,A,b);
-
+		if (res == 2)
+		{			
+			freeMatrix(A);
+			freeMatrix(b);
+			freeMatrix(x);
+			return EXIT_FAILURE;
+		}	
+		else if (res == 1)
+		{
+			freeMatrix(A);
+			freeMatrix(b);
+			freeMatrix(x);
+			return EXIT_FAILURE;
+		}	
 		printToScreen(x);
-	  freeMatrix(x);
+	  	freeMatrix(x);
 	} else {
 					fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
 	}
